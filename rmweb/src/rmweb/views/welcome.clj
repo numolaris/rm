ae4fe072402e0e1495b6faad4083cde726c3c1f3
Andrey <user@server>
2012-11-28 02:25:16 +0400

    test

--- rmweb/src/rmweb/views/welcome.clj
+++ rmweb/src/rmweb/views/welcome.clj
@@ -1,11 +1,17 @@
 (ns rmweb.views.welcome
-  (:require [rmweb.views.common :as common]
-            [noir.content.getting-started])
- (:use [noir.core :only [defpage]]))
+  (:require [rmweb.views.common :as common])
+    (:use [hiccup.page :only [include-css html5]])
+    (:use [hiccup.core])
+    (:use [hiccup.form]) )
 
 (defpage "/" []
-         (common/layout
-           [:p "Welcome to rmweb" :p "Welcome to rmweb2"]))
+        (common/layout
+            [:p "Welcome!"] 
+            (form-to [:get "/"]
+                (label "comment" "Comment: ")
+                (hidden-field "action" "add" )
+                (text-field "comment" )
+                (submit-button "Post"))))
 
 (defpage "/login" {:keys [username password]}
   (str "You tried to login as " username " with the password " password))
